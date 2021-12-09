a425edd3bbc03e46414f47bf4b1d8e9e55f779cd
hinerm <hinerm@gmail.com>
2021-12-09 09:43:08 -0600

    Try Popen

--- mproc.py
+++ mproc.py
@@ -16,13 +16,15 @@ def start():
     exit_poll.daemon = False
     # This daemon thread polling stdin blocks execution of subprocesses
     # But ONLY if running in another process with stdin connected to its parent by PIPE
-   # exit_poll.start()
+    exit_poll.start()
 
-    import multiprocessing as mp
-    queue = mp.Queue()
-    p = mp.Process(target=mpc.start, args=(queue,))
-    p.start()
-    print("result: ", queue.get())
+    import subprocess
+    m_worker = subprocess.Popen([sys.executable, "-u", "mproc_child.py"],
+            # When creating the subprocess with an open pipe to stdin and
+            # subsequently polling that pipe, it blocks further communication
+            # between subprocesses
+            stdin=subprocess.PIPE,
+            close_fds=False,)
     print("mproc done")
 
 if __name__ == '__main__':
--- mproc_child.py
+++ mproc_child.py
@@ -2,7 +2,9 @@ import multiprocessing as mp
 import time
 from multiprocessing.managers import BaseManager
 
-def start(queue):
-    queue.put("hello")
+def start():
+    print("helloooooo")
 
 
+if __name__ == '__main__':
+    start()
